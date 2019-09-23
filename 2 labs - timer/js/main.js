93238f39d3afddd06994064cf0949c15572b2830
sergeevev-git <sergeevev@gmail.com>
2019-09-24 03:52:15 +0500

    10st lab

--- 2 labs - timer/js/main.js
+++ 2 labs - timer/js/main.js
@@ -38,4 +38,48 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+//TIMER
+
+    let deadline = '2019-10-30';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()), //deadline minus cuurent time
+        //let t = Date.parse(endtime) - Date.parse(new Date()) - (5*3600*1000), //поправка на часовой пояс +5
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*60*60) % 24),
+            days = Math.floor(t/(1000*60*60*24));
+   
+        return {
+            'total' : t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            days.textContent = t.days;
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <=0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });

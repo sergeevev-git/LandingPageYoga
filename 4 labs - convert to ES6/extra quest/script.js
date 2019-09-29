3b0e4dfda197ab0138e1e9c4d8a695b5b67c7c4f
sergeevev-git <sergeevev@gmail.com>
2019-09-29 23:28:36 +0500

    lab 4-1: convert script to ES6 + refactoring of button 'more'

---
+++ 4 labs - convert to ES6/extra quest/script.js
@@ -0,0 +1,16 @@
+let age = document.getElementById('age');
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+}
+showUser('Иванов', 'Василий');
+
+
+// 'use strict'
+
+// function hello() {
+
+// console.log(this)
+
+// }
+
+// hello();
---
+++ 4 labs - convert to ES6/js/main.js
@@ -0,0 +1,141 @@
+// ожидает ПОЛНОЙ закгрузки страницы, вплоть до последней картинки
+// window.addEventListener('load')
+//
+//ожидает загрузки ДОМ дерева страницы
+window.addEventListener('DOMContentLoaded', function() {
+    
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    let hideTabContent = (a) => {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+
+        }
+    }
+
+    hideTabContent(1);
+
+    let showTabContent = (b) => {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+//TIMER
+
+    let deadline = '2019-10-5';
+
+    let getTimeRemaining = (endtime) => {
+        let t = Date.parse(endtime) - Date.parse(new Date()), //deadline minus cuurent time
+        //let t = Date.parse(endtime) - Date.parse(new Date()) - (5*3600*1000), //поправка на часовой пояс +5
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*60*60) % 24),
+            days = Math.floor(t/(1000*60*60*24));
+        return {
+            'total' : t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    let setClock = (id, endtime) => {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            let addZero = (time) =>{
+                if (time <= 9) {
+                    return '0' + time;
+                } else return time;
+            }
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+          
+            if (t.total <=0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+    // modal window
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+
+    // 1st option - просто обработчик событий массива кнопок
+    let tabBtn = document.querySelectorAll('.description-btn');
+
+    //отображение модального окна
+    let showModal = (a) => {
+        overlay.style.display = 'block';
+        a.classList.add('more-splash'); //анимация нажатой кнопки
+        document.body.style.overflow = 'hidden'; //отключения скроллинга страницы
+    }
+
+    tabBtn.forEach(function(item, i) {
+        item.addEventListener('click', function() {
+            showModal(tabBtn[i]);
+        });
+    });    
+    
+    more.addEventListener('click', function () {
+        showModal(more);
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+});
+
+
+
+// Второе задание
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+ 
+// showUser.apply(age, ["Горький","Максим"]);
+
